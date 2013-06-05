0e13ed7fdfd4710161ff345f444514d36dbaaca1
Lee Bolding <lee@leesbian.net>
2013-06-05 13:36:40 +0100

    for some reason, the classname doesn't match the filepath. that's a fail.

--- code/Core/Test/Model/Mysql4/Design/Theme/Collection.php
+++ code/Core/Test/Model/Mysql4/Design/Theme/Collection.php
@@ -7,20 +7,20 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Mage_Core_Test_Model_Mysql4_Design_Theme_Collection extends Mage_Test_Unit_Case
+class Mage_Core_Test_Model_Mysql4_Design_Theme extends Mage_Test_Unit_Case
 {
     /**
-     * @var Mage_Core_Model_Mysql4_Design_Theme_Collection
+     * @var Mage_Core_Model_Mysql4_Design_Theme
      */
     protected $_model;
 
     // constants required for integration tests
     const CLASS_GROUP_TYPE = parent::CLASS_GROUP_TYPE_MODEL;
-    const CLASS_ID = 'core/mysql4_design_theme_collection';
+    const CLASS_ID = 'core/mysql4_design_theme';
 
     protected function setUp()
     {
-        $this->_model = new Mage_Core_Model_Mysql4_Design_Theme_Collection();
+        $this->_model = new Mage_Core_Model_Mysql4_Design_Theme();
     }
 
     public function testLoad()
